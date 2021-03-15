8b2996cec396bc03f661a7218abd25cc60ad9d82
GuilhermeBeltrao <guilhermecezar.beltrao@gmail.com>
2021-03-15 18:25:19 -0300

    modified:   core/models.py 	modified:   core/views.py 	modified:   db.sqlite3 	modified:   projRAL/urls.py 	renamed:    templates/html -> templates/LoginPage 	renamed:    templates/css -> templates/LoginPageStyle

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Relators(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
