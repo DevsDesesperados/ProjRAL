aaa1b7fc9c6a77945a815a5616e8c0bc98f2c9c5
LatchedMicrobe0 <rodrigo.pontes.oliveira.lima@gmail.com>
2021-03-15 21:18:35 -0300

    Adicionando templates base do projeto

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login
 # Create your views here.
+
+def login_user(request):
+    return render(request, 'html-loginpage.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('Usuário, CPF ou CNPJ')
+        password = request.POST.get('Senha')
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            redirect('/')
+    else
+        redirect('/')
