820ca8b545e6adf1ebdddf9c644e0f066d48ec0b
LatchedMicrobe0 <rodrigo.pontes.oliveira.lima@gmail.com>
2021-03-18 10:51:49 -0300

    Primeira integração dos templates na interface do site

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth import login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-from django.http import HttpResponse
+def login_user(request):
+    return render(request,'login.html')
 
+@login_required(login_url='/login/')
+def menu(request):
+    return render(request, 'menu.html')
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
--- projRAL/urls.py
+++ projRAL/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('menu/', views.menu),
+    path('',RedirectView.as_view(url = '/menu/')),
+    path('login/',views.login_user)
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
